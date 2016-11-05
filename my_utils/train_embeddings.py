76165234ff147be2596ecc71eabdebf7e9b8e786
Ubuntu <ubuntu@ip-172-31-27-81.ec2.internal>
2016-11-05 23:26:28 +0000

    minor rev; added 'max_sent' argument to specify maximum number of sentences to be processed

--- my_utils/train_embeddings.py
+++ my_utils/train_embeddings.py
@@ -4,9 +4,11 @@ from ipdb import set_trace
 from __init__ import preprocess
 
 class GenReader(object):
-	def __init__(self, datasets):
+	def __init__(self, datasets, max_sent=None):
 		self.datasets = datasets
-
+		self.max_sent = max_sent if max_sent else float('inf')
+		if self.max_sent < float('inf'):
+			print "[max_sentences: %d]" % self.max_sent
 	def __iter__(self):
 		lines=0	
 		for dataset in self.datasets:
@@ -14,7 +16,7 @@ class GenReader(object):
 			with open(dataset) as fid:
 				for l in fid:		
 					lines+=1
-					# if lines>10: raise StopIteration				
+					if lines>self.max_sent: raise StopIteration			
 					p = preprocess(l.decode("utf-8"),sep_emoji=True)
 					yield p.split()
 
@@ -25,6 +27,7 @@ def get_parser():
     parser.add_argument('-out', type=str, required=True, help='path to store the embeddings')        
     parser.add_argument('-dim', type=int, default=400, help='size of embedding')        
     parser.add_argument('-workers', type=int, help='number of workers',default=4)        
+    parser.add_argument('-max_sent', type=int, help='set max number of sentences to be read (per file)')        
     
     return parser
 
@@ -32,9 +35,12 @@ if __name__ == "__main__":
 	
 	cmdline_parser = get_parser()
 	args = cmdline_parser.parse_args() 	
-	w2v = Word2Vec(sentences=GenReader(args.ds), size=args.dim, 
+	print "initializing..."
+	w2v = Word2Vec(sentences=GenReader(args.ds,args.max_sent), size=args.dim, 
 		           workers=args.workers, min_count=20, sg=1, hs=1)
-	w2v.train(GenReader(args.ds))
+	print "training..."
+	print w2v
+	w2v.train(GenReader(args.ds,args.max_sent))
 	w2v.save(args.out)
 	w2v.save_word2vec_format(args.out+".txt")
 	
