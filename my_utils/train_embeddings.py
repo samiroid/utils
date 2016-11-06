7303d32c978b408aad1c2643ebe1894064bf31eb
Ubuntu <ubuntu@ip-172-31-27-81.ec2.internal>
2016-11-06 01:24:12 +0000

    minor rev

--- my_utils/train_embeddings.py
+++ my_utils/train_embeddings.py
@@ -18,6 +18,10 @@ class GenReader(object):
 					lines+=1
 					if lines>self.max_sent: raise StopIteration			
 					p = preprocess(l.decode("utf-8"),sep_emoji=True)
+					#if p != l:
+					#	print p
+					#		print l
+					#		set_trace()
 					yield p.split()
 
 def get_parser():
@@ -31,19 +35,56 @@ def get_parser():
     
     return parser
 
+def reader(datasets,max_sent):
+	lines=0
+	for dataset in datasets:
+		print dataset
+		with open(dataset) as fid:
+			for l in fid:
+				lines+=1
+				if lines>max_sent: break
+				#print l
+				#continue
+				p = preprocess(l.decode("utf-8"),sep_emoji=True)
+				my_p = p.encode("utf-8").lower()
+				my_l = ' '.join(l.replace("\n","").lower().split())
+				#print type(my_p) == type(my_l)
+				if my_p != my_l: # and not "number" in my_p:
+					my_p_toks = my_p.split()
+					my_l_toks = my_l.split()
+					print "same size? ", len(my_p_toks) == len(my_l_toks)
+					for i in range(len(my_p_toks)):
+						if my_p_toks[i] != my_l_toks[i]:
+							print i, " ", my_p_toks[i], "==", my_l_toks[i], "? ", my_p_toks[i] == my_l_toks[i] 				
+##						if my_p_toks[i] != my_l_toks[i]:
+#					set_trace()	
+					
+					print my_p, my_p_toks
+					print my_l, my_l_toks
+					set_trace()
+				else:
+					print "good"
+
+#int p.split()
+
 if __name__ == "__main__":
 	
 	cmdline_parser = get_parser()
 	args = cmdline_parser.parse_args() 	
+	#reader(args.ds,5000)
+	#import sys; sys.exit()
 	print "initializing..."
+	print args.ds
 	w2v = Word2Vec(sentences=GenReader(args.ds,args.max_sent), size=args.dim, 
-		           workers=args.workers, min_count=20, sg=1, hs=1)
+		           workers=args.workers, min_count=20, sg=1, hs=1,iter=3)
 	print "training..."
 	print w2v
-	w2v.train(GenReader(args.ds,args.max_sent))
+	w2v.train(GenReader(args.ds,args.max_sent),iter=10)
 	w2v.save(args.out)
 	w2v.save_word2vec_format(args.out+".txt")
-	
+	print "Vocab:"
+	print w2v.vocab.keys()
+	print "Done"	
 	
 		
 
